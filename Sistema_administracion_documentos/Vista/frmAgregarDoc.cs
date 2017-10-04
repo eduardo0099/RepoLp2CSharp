81c4ca0516a185595f7ffd6b361eadb73dd5746d
negar14 <inoku_0099@hotmail.com>
2017-10-04 16:34:42 -0500

    cambio de form principal + inicio + documentos

---
+++ Sistema_administracion_documentos/Vista/frmAgregarDoc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class frmAgregarDoc : Form
+    {
+        public frmAgregarDoc()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmAgregarDoc_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtArchSelec.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
---
+++ Sistema_administracion_documentos/Vista/frmDocumentos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class frmDocumentos : Form
+    {
+        public frmDocumentos()
+        {
+            InitializeComponent();
+        }
+
+        public frmDocumentos(Form padre)
+        {
+            InitializeComponent();
+            this.MdiParent = padre;
+        }
+        private void frmDocumentos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            frmAgregarDoc FrmagregaDocumento = new frmAgregarDoc();
+            FrmagregaDocumento.ShowDialog();
+        }
+    }
+}
