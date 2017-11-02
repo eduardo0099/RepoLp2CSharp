7bccc7566dbf90d273a3cca25935b4a7ca34411a
negar14 <inoku_0099@hotmail.com>
2017-11-02 02:28:49 -0500

    anuncios

---
+++ Sistema_administracion_documentos/AccesoDatos/AnuncioDA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class AnuncioDA
+    {
+        private string cadenabd;
+        public AnuncioDA()
+        {
+            cadenabd = "server=200.16.7.96;user=inf282g5;database=inf282g5;port=3306;password=reFuKUxhUijfr8np;";
+        }
+
+
+
+    }
+}
